3ac2eab29ced54c79028d0f495a88f6052cc8306
Dipak Khade <dipakhade214@gmail.com>
2025-04-09 23:54:29 +0530

    sending zaps to kafka topic

--- apps/processer/index.ts
+++ apps/processer/index.ts
@@ -1,1 +1,36 @@
-console.log("Hello via Bun!");
+import { Kafka } from "kafkajs"
+import { prisma } from "db/client"
+
+const kafka = new Kafka({
+    clientId:'processer',
+    brokers:['localhost:9092']
+})
+
+const TOPIC_NAME = 'zapqueue';
+
+async function swipper(){
+    const producer  = kafka.producer();
+    await producer.connect();
+
+    while(true){
+
+        const zap_run_outbox = await prisma.zapRunOutBox.findMany({
+            where:{},
+            take:10,
+            select:{
+                zapRunId:true
+            }
+        })
+
+        await producer.send({
+            topic:TOPIC_NAME,
+            messages:zap_run_outbox.map(z=>{
+                    return {
+                        value:z.zapRunId
+                    }
+                })
+        })
+    }
+}
+
+swipper()
