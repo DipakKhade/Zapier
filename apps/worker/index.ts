6e65c3d0632d5a983e1a3d8724f4def9919a30ca
Dipak Khade <dipakhade214@gmail.com>
2025-04-10 22:43:26 +0530

    commting offset

--- apps/worker/index.ts
+++ apps/worker/index.ts
@@ -14,4 +14,19 @@ async function actionsConsumer(){
     await consumer.connect();
 
     consumer.subscribe({topic:TOPIC_NAME})
+
+    consumer.run({
+        autoCommit:false,
+        eachMessage:async({ topic, partition, message })=>{
+            console.log({partition,offset: message.offset,value: message.value!.toString()})
+              await consumer.commitOffsets([
+                {
+                    topic:TOPIC_NAME,
+                    offset:(parseInt(message.offset)+1).toString(),
+                    partition
+                }
+            ])
+        }
+    })
+    
 }
