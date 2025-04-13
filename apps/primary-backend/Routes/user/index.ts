88124e68e24e67309756b6b5435ba517672511c4
Dipak Khade <dipakhade214@gmail.com>
2025-04-13 17:58:33 +0530

    pri back changes

--- apps/primary-backend/Routes/user/index.ts
+++ apps/primary-backend/Routes/user/index.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { signInSchema } from "../../zodSchema/zod";
+import { signInSchema, signUpSchema } from "../../zodSchema/zod";
 import { prisma } from "db/client";
+import bcrypt from "bcrypt";
 
 const userRouter = Router();
+const SALT = 10;
 
 userRouter.post('/signin', async(req,res)=>{
     try{
@@ -17,10 +19,11 @@ userRouter.post('/signin', async(req,res)=>{
         }
         
         const { username, password } = parsedPaylaod.data
+        const hashed_password = bcrypt.hash(password, SALT)
         const new_user = await prisma.user.create({
             data:{
                 name:username,
-                password
+                password:hashed_password
             }
         })
 
@@ -35,12 +38,51 @@ userRouter.post('/signin', async(req,res)=>{
 })
 
 userRouter.post('/signup', async(req, res)=>{
+    try{
+        const { paylaod } = req.body;
+        const parsedPaylaod = signUpSchema.safeParse(paylaod);
     
+        if(parsedPaylaod.error){
+            res.status(411).json({
+                message:parsedPaylaod.error.message
+            })
+            return;
+        }
+    
+        const user = await prisma.user.findFirst({
+            where:{
+                name:parsedPaylaod.data.username
+            }
+        })
+    
+        if(!user){
+            res.status(404).json({
+                message:"no user found with username " + parsedPaylaod.data.username  
+            })
+            return;
+        }
+    
+        const is_password_valid = bcrypt.compare(parsedPaylaod.data.password, user.password);
+    
+        if(!is_password_valid){
+            res.status(200).json({
+                message:"wrong password"
+            })
+            return;
+        }
+    
+        res.json({
+            message:"user signup successfull",
+            success:true
+        })
+    }catch(error){
+        throw error
+    }
 
 })
 
 userRouter.get('/getUsers', async(req, res)=>{
-
+    
 })
 
 export default userRouter;
--- apps/primary-backend/Routes/zap/index.ts
+++ apps/primary-backend/Routes/zap/index.ts
@@ -1,9 +1,28 @@
 import { Router } from "express";
+import { authMiddleware } from "../../Middlewares/auth";
+import { prisma } from "db/client";
 
 const zapRouter = Router();
 
-zapRouter.get('/zapRuns', async(req,res)=>{
+zapRouter.get('/zapRuns', authMiddleware, async(req,res)=>{
+    try{
+        const zap_runs = await prisma.zap.findMany({
+            where:{
+                userId:req.userId
+            },
+            include:{
+                trigger:true,
+                action:true
+            }
+        })
 
+        res.json({
+            data:zap_runs
+        })
+        
+    }catch(error){
+        throw error
+    }
 })
 
 export default zapRouter;
