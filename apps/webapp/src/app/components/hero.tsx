ccd69097da1cd849c06e78724bb0bbcb6d1121e5
Dipak Khade <dipakhade214@gmail.com>
2025-04-16 21:28:21 +0530

    hero compo added

---
+++ apps/webapp/src/app/components/hero.tsx
@@ -0,0 +1,12 @@
+
+
+export const Hero = () =>{
+    return <div className="max-w-[40vw]">
+        <div className="font-extrabold text-8xl">
+        Automate without limits
+        </div>
+        <div className="font-medium text-2xl">
+        Turn chaos into smooth operations by automating workflows yourself—no developers, no IT tickets, no delays. The only limit is your imagination.
+        </div>
+    </div>
+}
