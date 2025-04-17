49c6c41205c293aea14cafa71cf57230c5bd1d2b
Dipak Khade <dipakhade214@gmail.com>
2025-04-17 17:19:34 +0530

    css header fix

--- apps/webapp/src/app/components/buttons.tsx
+++ apps/webapp/src/app/components/buttons.tsx
@@ -1,13 +1,13 @@
 import { ReactNode } from "react"
 
 export const PrimaryButton = ({ children, onClick } : { children:ReactNode, onClick? : ()=> void }) =>{
-    return <button onClick={onClick} className="bg-orange-500 rounded-3xl px-4 py-0 text-slate-50 font-semibold">
+    return <button onClick={onClick} className="bg-orange-500 rounded-3xl px-4 py-0 text-slate-50 font-semibold cursor-pointer">
         {children}
     </button>
 }
 
-export const HoverButton = ({ children, onClick } : { children: ReactNode, onClick: ()=> void}) =>{
-    return <button onClick={onClick}>
+export const HoverButton = ({ children, onClick } : { children: ReactNode, onClick?: ()=> void}) =>{
+    return <button onClick={onClick} className="hover:bg-slate-100 px-2 rounded-sm cursor-pointer">
         {children}
     </button>
 }
--- apps/webapp/src/app/components/header.tsx
+++ apps/webapp/src/app/components/header.tsx
@@ -1,14 +1,14 @@
-import { PrimaryButton } from "./buttons"
+import { HoverButton, PrimaryButton } from "./buttons"
 
 export const Header = () => {
     return <>
-        <div className="flex justify-around p-2 border border-b border-b-gray-400 h-16">
+        <div className="flex justify-around p-2 border border-b border-b-gray-400 h-16 z-10 fixed w-full bg-amber-50">
             <div className="text-3xl font-extrabold">
                 <span className="text-orange-500">_</span>Zapier
             </div>
 
             <div className="flex space-x-4">
-                <div>Log in</div>
+                <HoverButton>Log in</HoverButton>
                 <PrimaryButton>
                 Sign up
                 </PrimaryButton>
