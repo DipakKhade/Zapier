3c96f8f2e844913a9433755244f287368c5d57e2
Dipak Khade <dipakhade214@gmail.com>
2025-04-19 17:07:34 +0530

    sign in changes

--- apps/webapp/src/components/sign-up-sidecard.tsx
+++ apps/webapp/src/components/sign-up-sidecard.tsx
@@ -13,6 +13,6 @@ export const SignUpSideCard = () =>{
 }
 
 const CheckTick = () =>{
-    return <svg className="text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="20" width="20" size="20" color="StatusSuccessStronger" name="formCheckCircle"><path fill="#00FF00" d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM8.21 10.79l2.79 2.8 5.29-5.3 1.42 1.42-6.71 6.7-4.21-4.2 1.42-1.42Z"></path></svg>
+    return <svg className="text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" height="20" width="20" color="StatusSuccessStronger" name="formCheckCircle"><path fill="#00FF00" d="M12 22a10 10 0 1 0 0-20 10 10 0 0 0 0 20ZM8.21 10.79l2.79 2.8 5.29-5.3 1.42 1.42-6.71 6.7-4.21-4.2 1.42-1.42Z"></path></svg>
 }
 
