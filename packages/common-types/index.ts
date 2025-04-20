f3f9c7d2feae3b70967d6644c684a6164b380ac0
Dipak Khade <dipakhade214@gmail.com>
2025-04-20 22:34:52 +0530

    added common types package

---
+++ packages/common-types/index.ts
@@ -0,0 +1,34 @@
+import z from "zod";
+
+const signInSchema = z.object({
+    username:z.string().min(5),
+    email:z.string().email(),
+    password:z.string().min(5)
+})
+
+const signUpSchema = z.object({
+    username:z.string().min(5),
+    email:z.string().email(),
+    password:z.string().min(5)
+})
+
+const zapSchema = z.object({
+    availableTriggerId:z.string(),
+    actions:z.array(z.object({
+        availableActionId: z.string(),
+        metadata: z.any().optional()
+    }))
+})
+
+type signUpType = z.infer<typeof signUpSchema>
+type signInType = z.infer<typeof signInSchema>
+type zapType = z.infer<typeof zapSchema>
+
+export {
+    signInSchema,
+    signUpSchema,
+    zapSchema,
+    type signUpType,
+    type signInType, 
+    type zapType
+}
