443d63d43d57396bfee548732dd97f0c61251225
Dipak Khade <dipakhade214@gmail.com>
2025-04-26 20:07:18 +0530

    zap create page temp init

--- apps/webapp/src/components/sidebar.tsx
+++ apps/webapp/src/components/sidebar.tsx
@@ -1,15 +1,18 @@
+import Link from "next/link"
 import { PrimaryButton } from "./buttons"
 
 export const Sidebar = () =>{
 
     return <div className="h-screen w-72 bg-slate-50 border-r border-b border-gray-400 flex flex-col items-center">
         <div>
-            <PrimaryButton classNames="rounded-sm w-[250px] p-2 mt-3">
-                <div className="flex justify-center items-center space-x-1.5">
-                <span className="font-semibold text-2xl pr-2">+</span> 
-                <span className="font-semibold text-xl">Create</span>
-                </div>
-            </PrimaryButton>
+            <Link href={'/zap/create'}>
+                    <PrimaryButton classNames="rounded-sm w-[250px] p-2 mt-3">
+                        <div className="flex justify-center items-center space-x-1.5">
+                            <span className="font-semibold text-2xl pr-2">+</span> 
+                            <span className="font-semibold text-xl">Create</span>
+                        </div>
+                    </PrimaryButton>
+            </Link>
         </div>
     </div>
 }
