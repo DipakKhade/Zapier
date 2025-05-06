f45fe766f6f9a5ac5d69796f85c71a70e1f62909
Dipak Khade <dipakhade214@gmail.com>
2025-05-06 18:18:19 +0530

    dyn btn

--- apps/webapp/src/components/header.tsx
+++ apps/webapp/src/components/header.tsx
@@ -1,8 +1,19 @@
+'use client';
 
 import Link from "next/link"
 import { HoverButton, PrimaryButton } from "./buttons"
+import { useEffect, useState } from "react";
+import { useRouter } from "next/navigation";
 
 export const Header = () => {
+    const [token, setToken] = useState<string | null>(null);
+    const router = useRouter();
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        if (token) {
+            setToken(token);
+        }
+    },[token]);
     return <>
         <div className="flex justify-around p-2 border border-b border-b-gray-400 h-16 z-10 fixed w-full bg-amber-50">
             <div className="text-3xl font-extrabold">
@@ -11,7 +22,7 @@ export const Header = () => {
                 </Link>
             </div>
 
-            <div className="flex space-x-4">
+            {!token && <div className="flex space-x-4">
                 <HoverButton>
                     <Link href={'/login'}>
                     Log in
@@ -22,7 +33,21 @@ export const Header = () => {
                     Sign up
                     </Link>
                 </PrimaryButton>
-            </div>
+            </div>}
+
+            {
+                token && <div className="flex space-x-4">   
+                    <PrimaryButton onClick={()=>{
+                        localStorage.removeItem('token');
+                        setToken(null);
+                        router.push('/')
+                    }}>
+                        <Link href={'/'}>
+                        Log out
+                        </Link>
+                    </PrimaryButton>
+                    </div>
+            }
         </div>
     </>
 }
